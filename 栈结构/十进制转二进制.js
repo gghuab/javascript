09a573632d7bd2681b51f96564f567b3daa3ca4b
牛万里 <q1364748019@163.com>
2026-03-20 20:51:07 +0800

    尝试修改

--- 栈结构/十进制转二进制.js
+++ 栈结构/十进制转二进制.js
@@ -1,16 +1,17 @@
-import ArrayStack from './数组实现.js';
-const stack1 = new ArrayStack();
+import ArrayStack from './数组实现.js'
+const stack1 = new ArrayStack()
 function decToBir(num) {
-    while (num > 0) {
-        let dec = num % 2;
-        stack1.push(dec);
-        num = Math.floor(num / 2);
-    }
-    let bir = '';
-    while (!stack1.isEmpty()) {
-        bir += stack1.pop();
-    }
-    return bir;
+  while (num > 0) {
+    let dec = num % 2
+    stack1.push(dec)
+    num = Math.floor(num / 2)
+  }
+  let bir = ''
+  while (!stack1.isEmpty()) {
+    bir += stack1.pop()
+  }
+  return bir
 }
-console.log(decToBir(35));
-console.log(+'2');
+console.log(decToBir(35))
+console.log(+'2')
+console.log(+'2')
